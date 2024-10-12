0585f5bc154d800845e41e2cd9e65d6b5ea4d2e4
Andrey <Anarovetskii@gmail.com>
2024-10-12 23:17:11 +0400

    add Node

---
+++ LR2/src/main/java/ru/ssau/tk/_NAME_/_PROJECT_/functions/Node.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk._NAME_._PROJECT_.functions;
+
+public class Node
+{
+    Node next;
+    Node prev;
+    double x;
+    double y;
+    Node (double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
